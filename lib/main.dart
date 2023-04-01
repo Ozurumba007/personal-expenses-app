cc1e87c7b2ec17a6368a3e9e2b9d8b580225ae31
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-04-01 08:34:32 +0100

    styling the container widget

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyHomePage extends StatelessWidget {
     Transaction(
       id: 't2',
       title: 'Weekly Groceries',
-      amount: 20.50,
+      amount: 20.54,
       date: DateTime.now(),
     ),
   ];
@@ -60,8 +60,20 @@ class MyHomePage extends StatelessWidget {
                 child: Row(
                   children: [
                     Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
                       child: Text(
                         tx.amount.toString(),
+                        //
                       ),
                     ),
                     Column(
