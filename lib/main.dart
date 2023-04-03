d99ecee516b5372ebd7c29204233e910c0e1ab13
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-04-03 01:46:59 +0100

    removed the card widget from the main.dart and added it to new_transaction.dart

--- lib/main.dart
+++ lib/main.dart
@@ -49,41 +49,6 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              elevation: 5,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: titleController,
-                      // onChanged: (val) {
-                      //   titleInput = val;
-                      // },
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                      controller: amountController,
-                      // onChanged: (val) => amountInput = val,
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        print(titleController.text);
-                        // print(amountInput);
-                      },
-                      child: Text(
-                        'Add Transaction',
-                        style: TextStyle(
-                          color: Colors.purple,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
             TransactionList(),
           ],
         ),
